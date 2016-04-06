d6332c6d777d415f8a413bb48361b16902694ff8
Foskill <zhangjia517@hotmail.com>
2016-04-06 17:46:11 +0800

    Init

---
+++ BMI.py
@@ -0,0 +1,16 @@
+L=float(input('请输入你的身高：'))
+W=float(input('请输入你的体重：'))
+
+bmi =float(W/(L**2))
+
+print(bmi);
+if bmi > 32:
+    print("严重肥胖");
+elif bmi > 28:
+    print("肥胖");
+elif bmi > 25:
+    print("过重");
+elif bmi > 18.5:
+    print("正常");
+else:
+    print("轻");
---
+++ Hello.py
@@ -0,0 +1,11 @@
+#name = input("Please input your name: ")
+#print ("Hello! " + name + "!")
+
+#a = "ABC"
+#b = a
+#a = "XYZ"
+#print(b)
+
+print(ord('张'))
+print(ord('家'))
+print('张家'.encode('utf-8'))
